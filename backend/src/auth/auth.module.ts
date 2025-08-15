7a6c574a0c25c5b471634ce5b26b7227122177eb
AliMoradpour <mr.mrdp@yahoo.com>
2025-08-16 02:41:36 +0330

    feat: Implement access-token and refresh-token strategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,8 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module'; // Import UsersModule
+import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
@@ -9,9 +10,15 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [
     UsersModule,
     PassportModule,
+    // For Access Token
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'your-secret-key', // Secret key for JWT
-      signOptions: { expiresIn: '60s' }, // Token expiration time
+      secret: process.env.JWT_SECRET || 'your-access-secret-key',
+      signOptions: { expiresIn: '15m' },
+    }),
+    // For Refresh Token
+    JwtModule.register({
+      secret: process.env.JWT_REFRESH_SECRET || 'your-refresh-secret-key',
+      signOptions: { expiresIn: '7d' }, // 7 days expiration
     }),
   ],
   providers: [AuthService],
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,22 @@
+// src/auth/auth.service.ts
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload, {
+        secret: process.env.JWT_SECRET,
+        expiresIn: '15m',
+      }),
+      refresh_token: this.jwtService.sign(payload, {
+        secret: process.env.JWT_REFRESH_SECRET,
+        expiresIn: '7d',
+      }),
+    };
+  }
+}
