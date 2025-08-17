777fe2c1140bbc4477cc17c79ff9fe8cb7eeddee
AliMoradpour <mr.mrdp@yahoo.com>
2025-08-17 06:46:30 +0330

    feat: Serve static files from the uploads directory

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,30 +1,38 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { BlogModule } from './blog/blog.module';
-import { CategoriesService } from './categories/categories.service';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { CategoriesModule } from './categories/categories.module';
 import { UploadsModule } from './uploads/uploads.module';
+
+// NEW: ServeStaticModule
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Make ConfigModule available globally
+      isGlobal: true,
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    PrismaModule,
     AuthModule,
     UsersModule,
     CoursesModule,
     BlogModule,
     CategoriesModule,
-    PrismaModule,
     UploadsModule,
   ],
   controllers: [AppController],
-  providers: [AppService, CategoriesService],
+  providers: [AppService],
 })
 export class AppModule {}
